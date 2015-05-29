b2527f00d1c4e149d24609435b4e1c29eb23fae6
Cyril Legrand <cyril@sctr.net>
2015-05-29 19:32:15 +0200

    added counter table

--- config/schema.sql
+++ config/schema.sql
@@ -9,3 +9,13 @@ CREATE TABLE IF NOT EXISTS `user` (
   `enabled` enum('0','1') NOT NULL DEFAULT '0',
   PRIMARY KEY (`userId`)
 ) ENGINE=InnoDB  DEFAULT CHARSET=utf8;
+
+DROP TABLE IF EXISTS `counter`;
+CREATE TABLE `counter` (
+  `userId` int(10) unsigned NOT NULL,
+  `date` date NOT NULL,
+  `type` enum('pushups','squats','situps','jumpingjacks') NOT NULL,
+  `nb` smallint(4) unsigned NOT NULL DEFAULT '0',
+  PRIMARY KEY (`userId`,`date`,`type`),
+  CONSTRAINT `counter_ibfk_1` FOREIGN KEY (`userId`) REFERENCES `user` (`userId`) ON DELETE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
