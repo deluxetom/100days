76b685a4fdabf44f5478fa76b37d6c6c28502a94
Cyril Legrand <cyril@sctr.net>
2015-06-05 17:58:29 +0200

    bug fix wrong order

--- src/Controller/FeedController.php
+++ src/Controller/FeedController.php
@@ -28,7 +28,7 @@ class FeedController implements ControllerProviderInterface
 
         $series = $app['repository.series']->feed();
         for ($s=0;$s<count($series);$s++) {
-            $comments = $app['repository.comment']->findAll(['forDate'=>$series[$s]['date'], 'forUserId'=>$series[$s]['userId']],[],['timestamp'=>'ASC']);
+            $comments = $app['repository.comment']->findAll(['forDate'=>$series[$s]['date'], 'forUserId'=>$series[$s]['userId']],[],['timestamp'=>'DESC']);
             for ($c=0;$c<count($comments);$c++) {
                 $comments[$c]['user'] = $app['repository.user']->findByPk($comments[$c]['userId']);
             }
@@ -37,7 +37,7 @@ class FeedController implements ControllerProviderInterface
 
         $lastDate = [];
         $comments = [];
-        $tmpCom = $app['repository.comment']->findAll(['forDate'=>'0000-00-00'], [], ['timestamp'=>'ASC']);
+        $tmpCom = $app['repository.comment']->findAll(['forDate'=>'0000-00-00'], [], ['timestamp'=>'DESC']);
         for ($c=0;$c<count($tmpCom);$c++) {
             if (isset($comments[date("Y-m-d", strtotime($tmpCom[$c]['timestamp']))][$tmpCom[$c]['forUserId']])) {
                 $comments[date("Y-m-d", strtotime($tmpCom[$c]['timestamp']))][$tmpCom[$c]['forUserId']][] = $tmpCom[$c];
@@ -60,7 +60,9 @@ class FeedController implements ControllerProviderInterface
                     'timestamp' => $timestamp,
                     'comments' => [],
                 ];
-                foreach ($comments[$date][$forUserId] as $comment) {
+                $tmp = $comments[$date][$forUserId];
+                for ($i=count($tmp)-1;$i>=0;$i--) {
+                    $comment = $tmp[$i];
                     $user = $app['repository.user']->findByPk($comment['userId']);
                     $serie['comments'][] = ['user' => $user, 'comment' => $comment['comment']];
                 }
@@ -68,10 +70,10 @@ class FeedController implements ControllerProviderInterface
                 if (count($series) > 0) {
                     $tmp = [];
                     for ($s = 0; $s < count($series); $s++) {
-                        if ($s == 0 && $serie['timestamp'] < $series[$s]['timestamp']) {
+                        if ($s == 0 && $serie['timestamp'] > $series[$s]['timestamp']) {
                             $tmp[] = $serie;
                             $tmp[] = $series[$s];
-                        } else if (!isset($series[$s + 1]) || ($serie['timestamp'] >= $series[$s]['timestamp'] && $serie['timestamp'] < $series[$s + 1]['timestamp'])) {
+                        } else if ((isset($series[$s + 1]) && $serie['timestamp'] <= $series[$s]['timestamp'] && $serie['timestamp'] > $series[$s + 1]['timestamp']) || (!isset($series[$s + 1]) && $serie['timestamp'] <= $series[$s]['timestamp'])) {
                             $tmp[] = $series[$s];
                             $tmp[] = $serie;
                         } else {
@@ -84,7 +86,7 @@ class FeedController implements ControllerProviderInterface
                 }
             }
         }
-        
+
         return $app['twig']->render('feed.html.twig', array(
             'topY' => $topY,
             'topD' => $topD,
